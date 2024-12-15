51ce462e90c0904425906301e9542b0b5224af1e
Tim Jedrek <tim@timjedrek.com>
2024-12-14 22:10:01 -0600

    add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require("tailwindcss/colors");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
 	daisyui: {
 		themes: [
-			{
-				lofi: {
-					...require("daisyui/src/theming/themes")["lofi"],
-					"--rounded-box": "1rem",
-					"--rounded-btn": "0.5rem",
-					"--rounded-badge": "1.9rem"
-				},
-				black: {
-					...require("daisyui/src/theming/themes")["black"],
-					"--rounded-box": "1rem",
-					"--rounded-btn": "0.5rem",
-					"--rounded-badge": "1.9rem",
-					"secondary": "#e5e5e5"
-				}
-			}
-		],
+      {
+        lightMode: {
+          primary: colors.emerald[500],
+          secondary: colors.emerald[600],
+          accent: colors.emerald[400],
+          neutral: colors.emerald[800],
+          "base-100": colors.emerald[50], // Light mode background
+          "base-content": colors.emerald[900], // Light mode text
+        },
+      },
+      {
+        black: {
+          primary: colors.emerald[500],
+          secondary: colors.emerald[600],
+          accent: colors.emerald[400],
+          neutral: colors.emerald[100],
+          "base-100": colors.emerald[950], // Dark mode background
+          "base-content": colors.emerald[100], // Dark mode text
+        },
+      },
+    ],
 	},
 	theme: {
 		extend: {
