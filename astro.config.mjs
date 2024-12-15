e719e3259494ab6270b7f6e4e65c53b0d15c0c20
Tim Jedrek <tim@timjedrek.com>
2024-12-15 17:15:20 -0600

    more npm fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/static";
 
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
   output: "static",
-  adapter: netlify()
 });
